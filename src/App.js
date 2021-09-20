ca4df5606248f45443071fa7e4d476165cd455a8
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-20 10:56:16 -0600

    Code reset

--- src/App.js
+++ src/App.js
@@ -2,29 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
-const api = {
-  key: "0eaf38b50196fd6bfbe70c82c2411712",
-  base: "https://api.openweathermap.org/data/2.5",
-};
 
 function App() {
 
-
-const dateBuilder = (d) => {
-
-}
-
-
   return (
-    <div className="App">
-      <main>
-        <div className="search_box">
-          <input type="text" className="search_bar" placeholder="Search..." />
-        </div>
-        <div className="location">New york</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-      </main>
-    </div>
+  <div className='App'>
+<h1>Weather</h1>
+
+  </div>
   );
 }
 
