b1d3da146388f9863b9ee9a63945b218b9270fa1
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-20 10:16:32 -0600

    Updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, {useState,useEffect} from 'react';
-import './App.css';
-import WeatherDay from './components/WeatherDay';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
+const api = {
+  key: "0eaf38b50196fd6bfbe70c82c2411712",
+  base: "https://api.openweathermap.org/data/2.5",
+};
 
 function App() {
 
-useEffect(() => {
-fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=40.760780&lon=-111.891045&exclude={part}&appid=0eaf38b50196fd6bfbe70c82c2411712`)
-.then((res) => res.json())
-.then(res => console.log(res))
-}, [])
+
+const dateBuilder = (d) => {
+
+}
+
 
   return (
     <div className="App">
-   <h1>Weather Stuff</h1>
+      <main>
+        <div className="search_box">
+          <input type="text" className="search_bar" placeholder="Search..." />
+        </div>
+        <div className="location">New york</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+      </main>
     </div>
   );
 }
---
+++ src/utils/api.js
@@ -0,0 +1,4 @@
+export const API = {
+    key: '0eaf38b50196fd6bfbe70c82c2411712',
+    base: 'https://api.openweathermap.org/data/2.5'
+}
