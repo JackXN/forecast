24331d59551e4d885e6bf1cbc109351ec3a28010
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-20 10:58:17 -0600

    Creted initial state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import axios from "axios";
 
 function App() {
 
+// Initial state values
+const [latitude, setLatitude] = useState(0);
+const [longitude, setLongitude] = useState(0);
+const [weather,setWeather] = useState('');
+const [temp, setTemp] = useState(0);
+const [cityName, setCityName] = useState('');
+
+
+
+
+
+
+
   return (
   <div className='App'>
 <h1>Weather</h1>
